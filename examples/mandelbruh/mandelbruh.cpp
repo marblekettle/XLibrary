c535ecc67a59776e0f224b7e82d5a947b3013c15
marblekettle <tencivanni@gmail.com>
2023-03-01 10:36:06 +0100

    Moved base library to root of repo

--- examples/mandelbruh/mandelbruh.cpp
+++ examples/mandelbruh/mandelbruh.cpp
@@ -1,18 +1,22 @@
 #include "XLibrary.hpp"
 using namespace XLibrary;
 
+double	g_factor = 300.0;
+
 int	is_in_set(double x, double y) {
 	double a;
 	double b;
 
 	a = 0;
 	b = 0;
-	for (int iter = 0; iter < 192; iter++) {
+	for (int iter = 0; iter < 100; iter++) {
 		double t = a;
 		a = (a * a) - (b * b) + x;
 		b = (2 * t * b) + y;
 		if ((a * a) + (b * b) > 2.0)
 			return (255 - (4 * iter));
+		if ((a * a) + (b * b) < 0.01 && iter < 20)
+			break ;
 	}
 	return (0);
 }
@@ -21,8 +25,8 @@ void	mandel(XCanvas& canvas) {
 	for (int i = 0; i < 900; i++) {
 		for (int j = 0; j < 600; j++) {
 			double coords[2];
-			coords[0] = (i - 600) / 300.0;
-			coords[1] = (j - 300) / 300.0;
+			coords[0] = (i - 600) / g_factor;
+			coords[1] = (j - 300) / g_factor;
 			SDL_Color	color;
 			color.r = is_in_set(coords[0], coords[1]);
 			color.g = 0;
@@ -30,6 +34,20 @@ void	mandel(XCanvas& canvas) {
 			canvas.setPixel(i, j, color);
 		}
 	}
+}
+
+bool	zoom_in(void* canvas) {
+	g_factor *= 2.0;
+	mandel(*(reinterpret_cast<XCanvas*>(canvas)));
+	reinterpret_cast<XCanvas*>(canvas)->update();
+	return (true);
+}
+
+bool	zoom_out(void* canvas) {
+	g_factor *= 0.5;
+	mandel(*(reinterpret_cast<XCanvas*>(canvas)));
+	reinterpret_cast<XCanvas*>(canvas)->update();
+	return (true);
 }
 
 bool	exit_canvas(void* clock) {
@@ -48,6 +66,8 @@ int	main(int ac, char** av)
 	XClockSetting	setting;
 	XKeyMap	keymap;
 	keymap.addPressKey(SDL_SCANCODE_ESCAPE, exit_canvas, &clock);
+	keymap.addPressKey(SDL_SCANCODE_UP, zoom_in, &canvas);
+	keymap.addPressKey(SDL_SCANCODE_DOWN, zoom_out, &canvas);
 	keymap.linkSetting(setting);
 	clock.setSetting(setting);
 	clock.start();
